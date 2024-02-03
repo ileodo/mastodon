09da5bb053b32c7445c6619e0fecec4d839d76e2
iLeoDo <iLeoDo@Gmail.com>
2024-02-03 17:12:16 +0000

    Set MAX_MEDIA_ATTACHMENTS and ALLOW_MIX_MEDIA

--- app/models/media_attachment.rb
+++ app/models/media_attachment.rb
@@ -38,9 +38,9 @@ class MediaAttachment < ApplicationRecord
   enum processing: { queued: 0, in_progress: 1, complete: 2, failed: 3 }, _prefix: true
 
   MAX_DESCRIPTION_LENGTH = 1_500
-  MAX_MEDIA_ATTACHMENTS = 4
+  MAX_MEDIA_ATTACHMENTS = 16
 
-  ALLOW_MIX_MEDIA = false
+  ALLOW_MIX_MEDIA = true
 
   IMAGE_LIMIT = 16.megabytes
   VIDEO_LIMIT = 99.megabytes
